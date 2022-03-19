a32b42cd6219671ab0ac5477114faa9e21a80fe1
dddddkio <r4t5y6u@163.com>
2022-03-20 02:07:03 +0800

    答题页面初步

---
+++ components/questionSelection/questionSelection.js
@@ -0,0 +1,35 @@
+// components/questionSelection/questionSelection.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    questionNum: {
+      type: Number,
+      value: 0,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件加载完成后执行
+   */
+
+  ready: function () {
+    
+  },
+
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/module/module_MBTI/MBTI_quiz/MBTI_quiz.js
+++ pages/module/module_MBTI/MBTI_quiz/MBTI_quiz.js
@@ -1,6 +1,6 @@
 // pages/module/MBTI/MBTI_quiz/MBTI_quiz.js
 
-var util = require('../../../../utils/util');
+var mbti = require('../../../../utils/mbti');
 
 Page({
 
@@ -8,14 +8,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    now_index: 0,
+    showSelection: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(util)
+    var customResult = {}
+    for (let i in mbti) {
+      customResult[i] = 2
+    }
+    this.setData({
+      mbti,
+      customResult
+    })
   },
 
   /**
@@ -65,5 +73,34 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 下一个问题
+   */
+  changeQuiz: function (e) {
+    var now_index = this.data.now_index
+    if (e.currentTarget.id == "next") {
+      this.setData({
+        now_index: now_index + 1
+      })
+    }
+    if (e.currentTarget.id == "previous") {
+      this.setData({
+        now_index: now_index - 1
+      })
+    }
+  },
+
+
+  /**
+   * 显示答题卡
+   */
+  showSelection: function (e) {
+    var showSelection = this.data.showSelection
+    this.setData({
+      showSelection: !showSelection
+    })
   }
+
 })
