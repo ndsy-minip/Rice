5fc20a4c52d5d13d3e652852ca454df5d183dfeb
dddddkio <r4t5y6u@163.com>
2022-03-21 22:39:43 +0800

    add cloudfunctions

---
+++ Waking-the-Tiger-minip/utils/mbti.js
@@ -0,0 +1,493 @@
+module.exports =
+[{
+	'quiz_en': 'At a party do you',
+	'quiz_zh': '在聚会时，你通常',
+	'answera_en': 'Interact with many, including strangers',
+	'answera_zh': '与许多人交流，包括陌生人',
+	'answerb_en': 'Interact with a few, known to you',
+	'answerb_zh': '只和一些朋友交流'
+}, {
+	'quiz_en': 'Are you more',
+	'quiz_zh': '你更',
+	'answera_en': 'Realistic than speculative',
+	'answera_zh': '现实而非投机',
+	'answerb_en': 'Speculative than realistic',
+	'answerb_zh': '投机多于现实'
+}, {
+	'quiz_en': 'Is it worse to',
+	'quiz_zh': '对你来说什么更糟糕',
+	'answera_en': 'Have your "head in the clouds"',
+	'answera_zh': '白日做梦，心不在焉',
+	'answerb_en': 'Be"in a rut"',
+	'answerb_zh': '生活一成不变'
+}, {
+	'quiz_en': 'Are you more impressed by',
+	'quiz_zh': '哪个词语让你更印象深刻',
+	'answera_en': 'Principles',
+	'answera_zh': '原则',
+	'answerb_en': 'Emotions',
+	'answerb_zh': '情绪'
+}, {
+	'quiz_en': 'Are more drawn toward the',
+	'quiz_zh': '你更倾向于哪个词语',
+	'answera_en': 'Convincing',
+	'answera_zh': '令人信服的',
+	'answerb_en': 'Touching',
+	'answerb_zh': '令人鼓舞的'
+}, {
+	'quiz_en': 'Do you prefer to work',
+	'quiz_zh': '你更愿意工作到',
+	'answera_en': 'To deadlines',
+	'answera_zh': '期限的最后一刻',
+	'answerb_en': 'Just "whenever"',
+	'answerb_zh': '随便什么时候结束为止'
+}, {
+	'quiz_en': 'Do you tend to choose',
+	'quiz_zh': '在作选择时，你更倾向于',
+	'answera_en': 'Rather carefully',
+	'answera_zh': '非常小心的',
+	'answerb_en': 'Somewhat impulsively',
+	'answerb_zh': '有点冲动的'
+}, {
+	'quiz_en': 'At parties do you',
+	'quiz_zh': '在聚会上你会',
+	'answera_en': 'Stay late, with increasing energy',
+	'answera_zh': '精力越来越旺盛，待到很晚',
+	'answerb_en': 'Leave early with decreased energy',
+	'answerb_zh': '精力耗尽，早早离开'
+}, {
+	'quiz_en': 'Are you more attracted to',
+	'quiz_zh': '你更喜欢',
+	'answera_en': 'Sensible people',
+	'answera_zh': '理智的人',
+	'answerb_en': 'Imaginative people',
+	'answerb_zh': '富有想象力的人'
+}, {
+	'quiz_en': 'Are you more interested in',
+	'quiz_zh': '你对哪种更感兴趣',
+	'answera_en': 'What is actual',
+	'answera_zh': '实际是什么',
+	'answerb_en': 'What is possible',
+	'answerb_zh': '可能是什么'
+}, {
+	'quiz_en': 'In judging others are you more swayed by',
+	'quiz_zh': '在评判他人的时候，你更',
+	'answera_en': 'Laws than circumstances',
+	'answera_zh': '客观，不带个人感情色彩',
+	'answerb_en': 'Circumstances than laws',
+	'answerb_zh': '友善，有主观色彩'
+}, {
+	'quiz_en': 'In approaching others is your inclination to be somewhat',
+	'quiz_zh': '在接近他人时，你倾向于',
+	'answera_en': 'Objective',
+	'answera_zh': '带目的的',
+	'answerb_en': 'Personal',
+	'answerb_zh': '不带目的的'
+}, {
+	'quiz_en': 'Are you more',
+	'quiz_zh': '哪个词语更适合形容你',
+	'answera_en': 'Punctual',
+	'answera_zh': '守时的',
+	'answerb_en': 'Leisurely',
+	'answerb_zh': '悠闲的'
+}, {
+	'quiz_en': 'Does it bother you more having things',
+	'quiz_zh': '哪种情况更困扰你',
+	'answera_en': 'Incomplete',
+	'answera_zh': '未完成，保留再选择的可能',
+	'answerb_en': 'Completed',
+	'answerb_zh': '已完成，没有保留再选择的可能'
+}, {
+	'quiz_en': 'In your social groups do you',
+	'quiz_zh': '在你的社交圈里你',
+	'answera_en': "Keep abreast of other's happenings",
+	'answera_zh': '及时了解他人的动态',
+	'answerb_en': 'Get behind on the news',
+	'answerb_zh': '在消息上有些落后'
+}, {
+	'quiz_en': 'In doing ordinary things are you more likely to',
+	'quiz_zh': '在做一些常规的事情你会更喜欢',
+	'answera_en': 'Do it the usual way',
+	'answera_zh': '用平常的方法解决',
+	'answerb_en': 'Do it your own way',
+	'answerb_zh': '用自己的方式解决'
+}, {
+	'quiz_en': 'Writers should',
+	'quiz_zh': '写作的人应该',
+	'answera_en': '"Say what they mean and mean what they say"',
+	'answera_zh': '平铺直叙的',
+	'answerb_en': 'Express things more by use of analogy',
+	'answerb_zh': '使用比喻和象征的'
+}, {
+	'quiz_en': 'Which appeals to you more',
+	'quiz_zh': '哪种东西更吸引你',
+	'answera_en': 'Consistency of thought',
+	'answera_zh': '思想的一致',
+	'answerb_en': 'Harmonious human relationships',
+	'answerb_zh': '人际关系的和谐'
+}, {
+	'quiz_en': 'Are you more comfortable in making',
+	'quiz_zh': '哪种情形你更感舒适',
+	'answera_en': 'Logical judgements',
+	'answera_zh': '做批评、分析性判断时',
+	'answerb_en': 'Value judgements',
+	'answerb_zh': '做价值判断时'
+}, {
+	'quiz_en': 'Do you want things',
+	'quiz_zh': '你更希望事情',
+	'answera_en': 'Settled and decided',
+	'answera_zh': '被安排和决定',
+	'answerb_en': 'Unsettled and undecided',
+	'answerb_zh': '不被安排和决定'
+}, {
+	'quiz_en': 'Would you say you are more',
+	'quiz_zh': '你觉得你更',
+	'answera_en': 'Serious and determined',
+	'answera_zh': '认真而坚定',
+	'answerb_en': 'Easy-going',
+	'answerb_zh': '随和'
+}, {
+	'quiz_en': 'In phoning do you',
+	'quiz_zh': '在打电话的时候你会',
+	'answera_en': 'Rarely question that it will all be said',
+	'answera_zh': '想到什么说什么',
+	'answerb_en': "Rehearse what you'll say",
+	'answerb_zh': '提前想好你要说的话'
+}, {
+	'quiz_en': 'Facts',
+	'quiz_zh': '事实',
+	'answera_en': '"Speak for themselves"',
+	'answera_zh': '不用以阐述原则',
+	'answerb_en': 'Illustrate principles',
+	'answerb_zh': '用以阐述原则'
+}, {
+	'quiz_en': 'Are visionaries',
+	'quiz_zh': '你认为幻想家和理论家',
+	'answera_en': 'somewhat annoying',
+	'answera_zh': '有点烦人',
+	'answerb_en': 'rather fascinating',
+	'answerb_zh': '相当迷人'
+}, {
+	'quiz_en': 'Are you more often',
+	'quiz_zh': '你更经常是一个',
+	'answera_en': 'a cool-headed person',
+	'answera_zh': '头脑冷静的人',
+	'answerb_en': 'a warm-hearted person',
+	'answerb_zh': '热心肠的人'
+}, {
+	'quiz_en': 'Is it worse to be',
+	'quiz_zh': '更糟的是',
+	'answera_en': 'unjust',
+	'answera_zh': '不公正的',
+	'answerb_en': 'merciless',
+	'answerb_zh': '残忍的'
+}, {
+	'quiz_en': 'Should one usually let events occur',
+	'quiz_zh': '你认为事情应该怎样发生',
+	'answera_en': 'by careful selection and choice',
+	'answera_zh': '经过经营选择后',
+	'answerb_en': 'randomly and by chance',
+	'answerb_zh': '随机、偶然'
+}, {
+	'quiz_en': 'Do you feel better about',
+	'quiz_zh': '你感觉哪一个更好',
+	'answera_en': 'having purchased',
+	'answera_zh': '已经购买，没得选择',
+	'answerb_en': 'having the option to buy',
+	'answerb_zh': '还没购买，有几个选择'
+}, {
+	'quiz_en': 'In company do you',
+	'quiz_zh': '在工作中你更倾向于',
+	'answera_en': 'initiate conversation',
+	'answera_zh': '和同事发起对话',
+	'answerb_en': 'wait to be approached',
+	'answerb_zh': '等待同事接近'
+}, {
+	'quiz_en': 'Common sense is',
+	'quiz_zh': '你认为常识是',
+	'answera_en': 'rarely questionable',
+	'answera_zh': '通常是可信的',
+	'answerb_en': 'frequently questionable',
+	'answerb_zh': '时常可疑的'
+}, {
+	'quiz_en': 'Children often do not',
+	'quiz_zh': '一般来说，儿童',
+	'answera_en': 'make themselves useful enough',
+	'answera_zh': '没有发挥他们的用处',
+	'answerb_en': 'exercise their fantasy enough',
+	'answerb_zh': '没有发挥他们的想象力'
+}, {
+	'quiz_en': 'In making decisions do you feel more comfortable with',
+	'quiz_zh': '在做决定的时候什么让你感觉到更舒适',
+	'answera_en': 'standards',
+	'answera_zh': '标准',
+	'answerb_en': 'feelings',
+	'answerb_zh': '感觉'
+}, {
+	'quiz_en': 'Are you more',
+	'quiz_zh': '你认为自己',
+	'answera_en': 'firm than gentle',
+	'answera_zh': '强硬多于温柔',
+	'answerb_en': 'gentle than firm',
+	'answerb_zh': '温柔多于强硬'
+}, {
+	'quiz_en': 'Which is more admirable',
+	'quiz_zh': '哪一个更迷人',
+	'answera_en': 'the ability to organize and be methodical',
+	'answera_zh': '组织和有条不紊的能力',
+	'answerb_en': 'the ability to adapt and make do',
+	'answerb_zh': '适应的能力'
+}, {
+	'quiz_en': 'Do you put more value on',
+	'quiz_zh': '你更看重',
+	'answera_en': 'infinite',
+	'answera_zh': '严谨',
+	'answerb_en': 'open-minded',
+	'answerb_zh': '思想开放'
+}, {
+	'quiz_en': 'Does new and non-routine interaction with others',
+	'quiz_zh': '和陌生人交流你觉得',
+	'answera_en': 'stimulate and energize you',
+	'answera_zh': '有趣的，有必要的',
+	'answerb_en': 'tax your reserves',
+	'answerb_zh': '难堪的，没必要的'
+}, {
+	'quiz_en': 'Are you more frequently',
+	'quiz_zh': '你更经常是',
+	'answera_en': 'a practical sort of person',
+	'answera_zh': '脚踏实地的人',
+	'answerb_en': 'a fanciful sort of person',
+	'answerb_zh': '仰望星空的人'
+}, {
+	'quiz_en': 'Are you more likely to',
+	'quiz_zh': '对你来说哪种情况更容易',
+	'answera_en': 'see how others are useful',
+	'answera_zh': '更好地利用别人',
+	'answerb_en': 'see how others see',
+	'answerb_zh': '认同别人'
+}, {
+	'quiz_en': 'Which is more satisfying',
+	'quiz_zh': '你更喜欢',
+	'answera_en': 'to discuss an issue thoroughly',
+	'answera_zh': '彻底地讨论这件事情',
+	'answerb_en': 'to arrive at agreement on an issue',
+	'answerb_zh': '在某些问题上不一定要有结果'
+}, {
+	'quiz_en': 'Which rules you more',
+	'quiz_zh': '哪种东西更多地支配着你',
+	'answera_en': 'your head',
+	'answera_zh': '你的思想',
+	'answerb_en': 'your heart',
+	'answerb_zh': '你的感情'
+}, {
+	'quiz_en': 'Are you more comfortable with work that is',
+	'quiz_zh': '你更喜欢哪种工作',
+	'answera_en': 'contracted',
+	'answera_zh': '合同制的',
+	'answerb_en': 'done on a casual basis',
+	'answerb_zh': '随意完成的'
+}, {
+	'quiz_en': 'Do you tend to look for',
+	'quiz_zh': '你倾向于寻找',
+	'answera_en': 'the orderly',
+	'answera_zh': '有规律的东西',
+	'answerb_en': 'whatever turns up',
+	'answerb_zh': '任何出现的东西'
+}, {
+	'quiz_en': 'Do you prefer',
+	'quiz_zh': '你更喜欢',
+	'answera_en': 'many friends with brief contact',
+	'answera_zh': '许多有过短暂接触的朋友',
+	'answerb_en': 'a few friends with more lengthy contact',
+	'answerb_zh': '几个联系时间比较长的朋友'
+}, {
+	'quiz_en': 'Do you go more by',
+	'quiz_zh': '你更倾向于',
+	'answera_en': 'facts',
+	'answera_zh': '现实',
+	'answerb_en': 'principles',
+	'answerb_zh': '原则'
+}, {
+	'quiz_en': 'Are you more interested in',
+	'quiz_zh': '你对哪个更有兴趣',
+	'answera_en': 'production and distribution',
+	'answera_zh': '生产和分销',
+	'answerb_en': 'design and research',
+	'answerb_zh': '设计和研究'
+}, {
+	'quiz_en': 'Which is more of a compliment',
+	'quiz_zh': '以下哪种赞美语更好',
+	'answera_en': '"There is a very logical person"',
+	'answera_zh': '这是一个非常有逻辑的人',
+	'answerb_en': '"There is a very sentimental person"',
+	'answerb_zh': '这是一个情感丰富的人'
+}, {
+	'quiz_en': 'Do you value in yourself more that you are',
+	'quiz_zh': '你更希望自己',
+	'answera_en': 'unwavering',
+	'answera_zh': '坚定不移的',
+	'answerb_en': 'devoted',
+	'answerb_zh': '投入的'
+}, {
+	'quiz_en': 'Do you more often prefer the',
+	'quiz_zh': '你通常更喜欢',
+	'answera_en': 'final and unalterable statement',
+	'answera_zh': '正式签署的、存档的',
+	'answerb_en': 'tentative and preliminary statement',
+	'answerb_zh': '只是口头协议'
+}, {
+	'quiz_en': 'Are you more comfortable',
+	'quiz_zh': '哪种情况你更感舒适',
+	'answera_en': 'after a decision',
+	'answera_zh': '作出决定之后',
+	'answerb_en': 'before a decision',
+	'answerb_zh': '作出决定之前'
+}, {
+	'quiz_en': 'Do you',
+	'quiz_zh': '和陌生人交流时你觉得',
+	'answera_en': 'speak easily and at length with strangers',
+	'answera_zh': '轻松',
+	'answerb_en': 'find little to say to strangers',
+	'answerb_zh': '费劲'
+}, {
+	'quiz_en': 'Are you more likely to trust your',
+	'quiz_zh': '你更相信你的',
+	'answera_en': 'experience',
+	'answera_zh': '经验',
+	'answerb_en': 'hunch',
+	'answerb_zh': '想象'
+}, {
+	'quiz_en': 'Do you feel',
+	'quiz_zh': '你更像哪种人',
+	'answera_en': 'more practical than ingenious',
+	'answera_zh': '事实性多于思辨性',
+	'answerb_en': 'more ingenious than practical',
+	'answerb_zh': '思辨性多于事实性'
+}, {
+	'quiz_en': 'Which person is more to be complimented one of',
+	'quiz_zh': '你觉得哪种人更值得被称赞',
+	'answera_en': 'clear reason',
+	'answera_zh': '理智的',
+	'answerb_en': 'strong feeling',
+	'answerb_zh': '热枕的'
+}, {
+	'quiz_en': 'Are you inclined more to be',
+	'quiz_zh': '哪种更好些',
+	'answera_en': 'fair-minded',
+	'answera_zh': '公正的',
+	'answerb_en': 'sympathetic',
+	'answerb_zh': '仁慈的'
+}, {
+	'quiz_en': 'Is it preferable mostly to',
+	'quiz_zh': '大多数情况下，你更愿意',
+	'answera_en': 'make sure things are arranged',
+	'answera_zh': '确信事情安排妥当',
+	'answerb_en': 'just let things happen',
+	'answerb_zh': '任由事情发生'
+}, {
+	'quiz_en': 'In relationships should most things be',
+	'quiz_zh': '在人际关系中，大多数事情都应该是',
+	'answera_en': 're-negotiable',
+	'answera_zh': '可控制的',
+	'answerb_en': 'random and circumstantial',
+	'answerb_zh': '随机和间接的'
+}, {
+	'quiz_en': 'When the phone rings do you',
+	'quiz_zh': '当电话铃声响起时，你会',
+	'answera_en': 'hasten to get to it first',
+	'answera_zh': '很快去接听',
+	'answerb_en': 'hope someone else will answer',
+	'answerb_zh': '希望别人去接听'
+}, {
+	'quiz_en': 'Do you prize more in yourself',
+	'quiz_zh': '你更希望你自己是',
+	'answera_en': 'a strong sense of reality',
+	'answera_zh': '有强烈的现实感的人',
+	'answerb_en': 'a vivid imagination',
+	'answerb_zh': '有生动的想象力的人'
+}, {
+	'quiz_en': 'Are you drawn more to',
+	'quiz_zh': '哪个词语更吸引你',
+	'answera_en': 'fundamentals',
+	'answera_zh': '基本规则',
+	'answerb_en': 'overtones',
+	'answerb_zh': '言外之意'
+}, {
+	'quiz_en': 'Which seems the greater error',
+	'quiz_zh': '哪一个词语更糟糕',
+	'answera_en': 'to be too passionate',
+	'answera_zh': '绝对主观的',
+	'answerb_en': 'to be too objective',
+	'answerb_zh': '绝对客观的'
+}, {
+	'quiz_en': 'Do you see yourself as basically',
+	'quiz_zh': '你认为自己基本上是一个',
+	'answera_en': 'hard-headed',
+	'answera_zh': '硬心肠的人',
+	'answerb_en': 'soft-hearted',
+	'answerb_zh': '温柔的人'
+}, {
+	'quiz_en': 'Which situation appeals to you more',
+	'quiz_zh': '你更喜欢哪种情况',
+	'answera_en': 'the structured and scheduled',
+	'answera_zh': '有条理的计划好的',
+	'answerb_en': 'the unstructured and unscheduled',
+	'answerb_zh': '没计划好的'
+}, {
+	'quiz_en': 'Are you a person that is more',
+	'quiz_zh': '你更像哪种人',
+	'answera_en': 'routinized than whimsical',
+	'answera_zh': '例行常规多于一时兴起',
+	'answerb_en': 'whimsical than routinized',
+	'answerb_zh': '一时兴起多于例行常规'
+}, {
+	'quiz_en': 'Are you more inclined to be',
+	'quiz_zh': '你认为你自己是',
+	'answera_en': 'easy to approach',
+	'answera_zh': '容易接近的',
+	'answerb_en': 'somewhat reserved',
+	'answerb_zh': '有点矜持的'
+}, {
+	'quiz_en': 'In writings do you prefer',
+	'quiz_zh': '在写作的时候你更喜欢',
+	'answera_en': 'the more literal',
+	'answera_zh': '写实',
+	'answerb_en': 'the more figurative',
+	'answerb_zh': '写意'
+}, {
+	'quiz_en': 'Is it harder for you to',
+	'quiz_zh': '对你来说那种情况更难',
+	'answera_en': 'identify with others',
+	'answera_zh': '认同他人',
+	'answerb_en': 'utilize others',
+	'answerb_zh': '利用他人'
+}, {
+	'quiz_en': 'Which do you wish more for yourself',
+	'quiz_zh': '你更希望自己',
+	'answera_en': 'clarity of reason',
+	'answera_zh': '明辨是非',
+	'answerb_en': 'strength of compassion',
+	'answerb_zh': '富有同情心'
+}, {
+	'quiz_en': 'Which is the greater fault',
+	'quiz_zh': '你认为怎么样更不好',
+	'answera_en': 'being indiscriminate',
+	'answera_zh': '轻率的',
+	'answerb_en': 'being critical',
+	'answerb_zh': '挑剔的'
+}, {
+	'quiz_en': 'Do you prefer the',
+	'quiz_zh': '在工作中，你希望你的任务是',
+	'answera_en': 'planned event',
+	'answera_zh': '事先排定的',
+	'answerb_en': 'unplanned event',
+	'answerb_zh': '没有排定的'
+}, {
+	'quiz_en': 'Do you tend to be more',
+	'quiz_zh': '哪一个更符合你',
+	'answera_en': 'deliberate than spontaneous',
+	'answera_zh': '审慎多于自发（言行谨慎多于信口随心）',
+	'answerb_en': 'spontaneous than deliberate',
+	'answerb_zh': '自发多于审慎（信口随心多于言行谨慎）'
+}]
+
