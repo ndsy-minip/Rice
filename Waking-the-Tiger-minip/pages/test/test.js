798370e5eeb8e5bce655f2530e540ec4a8550d32
dddddkio <r4t5y6u@163.com>
2022-03-27 01:25:37 +0800

    timer（template in test page）

--- Waking-the-Tiger-minip/pages/test/test.js
+++ Waking-the-Tiger-minip/pages/test/test.js
@@ -1,32 +1,94 @@
-// Waking-the-Tiger-minip/pages/test/test.js
-// pages/prize/prize.js
-const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    motto: 'Hello World',
-    hidden: true,
-    userInfo: {},
-    hasUserInfo: false,
-    windowWidth: '',
-    posterHeight: '',
+    timer: '',//定时器名字
+    countDownNum: '1440',//倒计时初始值
+    timer_M: '24',//m
+    timer_S: '00',//s
+    suspendButton: '暂停',//暂停按钮标题
+    isSuspend: '0' //是否暂停
   },
-  onLoad: function () {
-    const poster = {
-      "with": 375,
-      "height": 587
-    }
-    const systemInfo = wx.getSystemInfoSync()
-    let windowWidth = systemInfo.windowWidth
-    let windowHeight = systemInfo.windowHeight
-    let posterHeight = parseInt((windowWidth / poster.with) * poster.height)
-    this.setData({
-      windowWidth: windowWidth,
-      posterHeight: posterHeight
+
+  onShow: function () {
+    
+  },
+
+  //开始
+  setStart(e) {
+    this.countDown();
+    wx.showToast({
+      title: '开始',
+      icon: 'success',
+      duration: 1000
     })
   },
-  
+
+  //暂停-继续
+  setSuspend(e) {
+    let that = this;
+    let isSuspend = that.data.isSuspend;
+    let suspendButton = that.data.suspendButton;//暂停按钮标题
+    if (isSuspend == 0){
+      isSuspend = '1';
+      suspendButton = "继续";
+    }else{
+      isSuspend = '0';
+      suspendButton = "暂停";
+    }
+    this.setData({
+      isSuspend: isSuspend,//是否暂停
+      suspendButton: suspendButton
+    })
+  },
+
+  //停止
+  setStop(e) {
+    let that = this;
+    clearInterval(that.data.timer);
+    this.setData({
+      timer: '',//定时器名字
+      countDownNum: '1440',//倒计时初始值
+      timer_M: '24',//m
+      timer_S: '00',//s
+      suspendButton: '暂停',//s
+    })
+  },
+  countDown: function () {
+    let that = this;
+    var m, s;
+    let countDownNum = that.data.countDownNum;
+    that.setData({
+      timer: setInterval(function () {
+
+        let isSuspend = that.data.isSuspend;
+        if (isSuspend == 0){
+
+          countDownNum--;
+
+          m = Math.floor(countDownNum / 60);
+          s = Math.floor(countDownNum % 60);
+          if (m < 10) {
+            m = "0" + m;
+          }
+          if (s < 10) {
+            s = "0" + s;
+          }
+
+          that.setData({
+            countDownNum: countDownNum,
+            timer_M: m,
+            timer_S: s
+          })
+          if (countDownNum == 0) {
+            clearInterval(that.data.timer);
+            that.setData({
+              countDownNumString: '截止'
+            })
+          }
+        }
+      }, 1000)
+    })
+  }
 })
--- Waking-the-Tiger-minip/pages/timer/timer.js
+++ Waking-the-Tiger-minip/pages/timer/timer.js
@@ -1,27 +1,26 @@
-// pages/index.js
+// pages/timer/timer.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    swiperList: [{
-      id: 0,
-      type: 'image',
-      url: 'cloud://cloud1-3gp8ynahdb4ea1a5.636c-cloud1-3gp8ynahdb4ea1a5-1310409999/bg-test.png'
-    },
-    {
-      id: 1,
-      type: 'image',
-      url: 'cloud://cloud1-3gp8ynahdb4ea1a5.636c-cloud1-3gp8ynahdb4ea1a5-1310409999/bg-test.png'
-    }]
+    multiIndex: [0, 0],
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var multiArray = [[], []]
+    for (var i = 0; i < 60; i++) {
+      multiArray[0].push(i.toString());
+      multiArray[1].push(i.toString());
+    }
+    this.setData({
+      multiArray
+    })
   },
 
   /**
@@ -73,22 +72,20 @@ Page({
 
   },
 
+  /**
+   * 计算时间
+   */
 
   /**
-   * 获取用户信息
+   * 设置时间
    */
-  getUserProfile(e) {
-    wx.getUserProfile({
-      desc: '用于在个人页展示头像、昵称', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        wx.setStorageSync('userInfo', res.userInfo)
-        // wx.switchTab({
-        //   url: '../module/module_myPage/profile/profile',
-        // })
-        wx.navigateTo({
-          url: '../test/test',
-        })
-      }
+  bindMultiPickerChange: function (e) {
+    this.setData({
+      multiIndex: e.detail.value
     })
+    var multiArray = this.data.multiArray
+    var multiIndex = this.data.multiIndex
+    var sec = parseInt(multiArray[0][multiIndex[0]])*60 + parseInt(multiArray[1][multiIndex[1]])
+    console.log(sec)
   },
 })
